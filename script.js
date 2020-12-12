df88e46e7334cf142ac9aff2bffd11014fec47d6
Ines Radic <inesradic12314@gmail.com>
2020-12-11 20:10:55 -0800

    add pseudo code and time variable

---
+++ script.js
@@ -0,0 +1,18 @@
+// Array for hours
+var displayTime = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+
+// Display current date for the calendar
+
+// Call function to display the current date
+
+// Function to display scheduler structure and format
+
+// For loop to display css structure for the columns and rows.
+
+// Textarea color transition for past, present, and future hours (grabs variable using template literals).
+
+// Call function to display structure of page
+
+// Save button is clicked, typed notes saved to local storage.
+
+
