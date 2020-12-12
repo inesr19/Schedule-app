6aa0151089201e97ffd5eaca4407a17df9fd3a43
Ines Radic <inesradic12314@gmail.com>
2020-12-11 20:22:34 -0800

    add save to local storage on click function

--- script.js
+++ script.js
@@ -11,31 +11,38 @@ displayDate();
 
 // Function to display scheduler structure and format
 function renderPage() {
-// For loop to display css structure for the columns and rows.
-for (let i = 9; i < 18; i++) {
-    $('<div>', {
-        id: `hour-${i}`,
-        class: 'row time-block past'
-    }).append($('<div>', {
-        class: 'col-md-1 hour',
-        text: `${displayTime[i - 9]}`
-    })).append($('<textarea>', {
-        class: 'col-md-10 description'
-    })).append($('<button>', {
-        class: 'button saveBtn col-md-1',
-        text: 'Save'
-    })).appendTo('.container');
+    // For loop to display css structure for the columns and rows.
+    for (let i = 9; i < 18; i++) {
+        $('<div>', {
+            id: `hour-${i}`,
+            class: 'row time-block past'
+        }).append($('<div>', {
+            class: 'col-md-1 hour',
+            text: `${displayTime[i - 9]}`
+        })).append($('<textarea>', {
+            class: 'col-md-10 description'
+        })).append($('<button>', {
+            class: 'button saveBtn col-md-1',
+            text: 'Save'
+        })).appendTo('.container');
 
-// Textarea color transition for past, present, and future hours (grabs variable using template literals).
- if (moment().format('H') == i) {
+        // Textarea color transition for past, present, and future hours (grabs variable using template literals).
+        if (moment().format('H') == i) {
             $(`#hour-${i}`).addClass('present');
         } else if (moment().format('H') > i) {
             $(`#hour-${i}`).addClass('past');
         } else {
             $(`#hour-${i}`).addClass('future');
         }
-// Call function to display structure of page
     }
-// Save button is clicked, typed notes saved to local storage.
-    
+    // Save button is clicked, typed notes saved to local storage.
+    $('.saveBtn').on('click', function () {
+        var input = $(`#hour-$`).attr(`${displayTime[i - 9]}`);
+        var output = $(this).siblings('textarea').val();
+        localStorage.setItem(input, output);
+    });
+    var input = `${displayTime[i - 9]}`;
+    localStorage.getItem(input);
 }
+// Call function to display structure of page
+renderPage();
