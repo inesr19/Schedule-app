bcea36efbe5b7ff151f54fbe95b02f246b814dd0
Ines Radic <inesradic12314@gmail.com>
2020-12-11 21:53:30 -0800

    update function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Worked with Rhea Reyes 
-
 // Array for hours
 var displayTime = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
 
@@ -10,6 +8,9 @@ function displayDate() {
 }
 // Call function to display the current date
 displayDate();
+
+
+
 
 // Function to display scheduler structure and format
 function renderPage() {
@@ -28,6 +29,13 @@ function renderPage() {
             text: 'Save'
         })).appendTo('.container');
 
+        // Save button is clicked, typed notes saved to local storage.
+        $('.button').on('click', function () {
+            var input = `${displayTime[i - 9]}`;
+            var output = $(this).siblings('textarea').val();
+            localStorage.setItem(input, output);
+
+        });
         // Textarea color transition for past, present, and future hours (grabs variable using template literals).
         if (moment().format('H') == i) {
             $(`#hour-${i}`).addClass('present');
@@ -36,16 +44,9 @@ function renderPage() {
         } else {
             $(`#hour-${i}`).addClass('future');
         }
-        // Save button is clicked, typed notes saved to local storage.
-        $('.button').on('click', function () {
-            var input = `${displayTime[i-9]}`;
-            var output = $(this).siblings('textarea').val();
-            localStorage.setItem(input, output);
-
-            localStorage.getItem(input);
-        });
+        var input = `${displayTime[i - 9]}`;
+        localStorage.getItem(input);
     }
-
 }
 // Call function to display structure of page
 renderPage();
