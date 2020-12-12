68a2d1412a05c898a2222eaa6e2f01bef2da68cf
Ines Radic <inesradic12314@gmail.com>
2020-12-11 20:12:07 -0800

    add moment.js to display current day

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 var displayTime = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
 
 // Display current date for the calendar
+function displayDate() {
+    var currentTime = moment().format("MMM Do YY");
+    $('#currentDay').text(currentTime);
+}
+// Call function to display the current date
+displayDate();
 
 // Call function to display the current date
 
